cd66c1675c7e5ce9f13ca83f07e2d0dee2044543
dofes <91889957+dofes@users.noreply.github.com>
2023-08-13 15:13:38 +0800

    a

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -17,5 +17,25 @@ extern Logger logger;
 void PluginInit()
 {
     // Your code here
-    logger.info("Hello, world!");
+
+    logger.info("loaded!");
+    logger.info("Author: Dofes");
+}
+
+#include <llapi/mc/Actor.hpp>
+#include <llapi/mc/AbsorptionMobEffect.hpp>
+#include <llapi/mc/MobEffect.hpp>
+#include <llapi/mc/SharedAttributes.hpp>
+#include <llapi/mc/AttributeInstance.hpp>
+THook(void, "?updateEffects@MobEffect@@UEAAXPEAVActor@@HH@Z",
+      MobEffect *_this,
+      Actor *target,
+      int durationTicks,
+      int amplification)
+{
+    if (_this->getId() == (int)MobEffect::EffectType::Absorption)
+    {
+        target->getMutableAttribute(SharedAttributes::ABSORPTION)->resetToDefaultValue();
+    }
+    original(_this, target, durationTicks, amplification);
 }
